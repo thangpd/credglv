4eebd65aaaae7919d3ce220a56084c8a205220b9
thangpd <5cm.1s.takaki@gmail.com>
2019-04-12 16:53:44 +0700

    change referal header

--- glv-multilevel-commision/views/front/myaccount-referral.php
+++ glv-multilevel-commision/views/front/myaccount-referral.php
@@ -1,47 +1,24 @@
 <div class="referral_program_details">
+    <span class="referral_icon"></span>
+    <span><?php _e( 'Referral Link', 'wmc' ); ?></span>
 	<?php
-	if( isset( $data['active'] ) && $data['active'] == 'amount_tab' ){ ?>
-	<div class="referral_program_overview second_section">
 
-		<div class="referral_program_stats total_earn_credit">
-			<span class="total_credit_icon"></span>
-			<span><?php _e('Total Credits Earned','woocommerce-extention'); ?></span>
-			<span class="show_output"><?php echo  floor($data['total_earn_point']) ; ?></span>
-		</div>
-		<div class="referral_program_stats total_avilable_credit">
-			<span class="total_credit_icon"></span>
-			<span><?php _e('Total Credits Available','wmc'); ?></span>
-			<span class="show_output"><?php echo  floor($data['total_points']); ?></span>
-		</div>
-		<div class="referral_program_stats">
-			<span class="total_credit_icon total_withdraw_credit"></span>
-			<span><?php echo _e('Total Withdrawn','woocommerce-extention'); ?></span>
-			<span class="show_output"><?php echo  floor($data['total_withdraw']); ?></span>
-		</div>
-	</div>
-	<?php }else{?>
-	<div class="referral_program_overview referral_top_section">
-		<div class="referral_program_stats">
-			<span class="referral_icon"></span>
-			<span><?php _e('Referral Code', 'wmc'); ?></span>
-			<span class="show_output"><?php echo $data['referral_code']; ?></span>
-		</div>
-		<div class="referral_program_stats total_avilable_credit">
-			<span class="total_credit_icon"></span>
-			<span><?php _e('Total Credits Available','wmc'); ?></span>
-			<span class="show_output"><?php echo  floor($data['total_points']) ; ?></span>
-		</div>
-		<div class="referral_program_stats">
-			<span class="total_referral"></span>
-			<span><?php _e('Total Referrals', 'wmc'); ?></span>
-			<span class="show_output"><?php echo $data['total_followers']; ?></span>
-		</div>
-	</div>
-	<?php } ?>
-	<div class="referral_program_sections" style="padding-top: 30px;">
+	$link_referral = '';
 
-		<div class="referral_program_content">
+	if ( $myaccount_id = get_option( 'woocommerce_myaccount_page_id' ) ) {
+		$link_referral = get_permalink( $myaccount_id ) . '?ru=' . $data['referral_code'];
+	} else {
+		$link_referral = home_url() . '?ru=' . $data['referral_code'];
+	}
+	?>
+    <input type="text" value="<?php echo $link_referral; ?>">
+    <span class="total_referral"></span>
+    <span><?php _e( 'Total Referrals: ', 'wmc' ); ?></span>
+    <span class="show_output"><?php echo $data['total_followers']; ?></span>
+    <div class="referral_program_sections" style="padding-top: 30px;">
+
+        <div class="referral_program_content">
 			<?php echo $data['content']; ?>
-		</div>
-	</div>
+        </div>
+    </div>
 </div>
