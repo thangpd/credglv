ad641d6dd06c1bfb2fdf04ac9e47338a85ade3b7
Khoa Vu <nhockunbi131097@gmail.com>
2019-05-06 16:10:47 +0700

    update

--- credglv/front/views/user/profile.php
+++ credglv/front/views/user/profile.php
@@ -1,78 +1,4 @@
 <?php
-if (isset($_POST['uploadclick'])) {
-    $user_id = get_current_user_id();
-    if ($_FILES['user_passports']['error'] <= 0) {
-        $type_pp = $_FILES['user_passports']['type'];
-        $explode = explode('/', $type_pp);
-        if ($explode[0] == 'image') {
-            $from_pp = $_FILES['user_passports']['tmp_name'];
-            $to = wp_upload_dir()['basedir'];
-            $timezone  = +7;
-            $time = gmdate("Y-m-j-H-i-s", time() + 3600*($timezone+date("I")));
-            $name_pp = $user_id . '_pp_' .$time.'_'. $_FILES['user_passports']['name'];
-            $url = wp_upload_dir()['baseurl'] . '/credglv/img/' . $name_pp;
-            $src = $to . '/credglv/img/' . $name_pp;
-            if (!is_file($src)) {
-                move_uploaded_file($from_pp, $src);
-                update_user_meta($user_id, 'passports', $url);
-            }
-        }
-    }
-    if ($_FILES['user_indentification']['error'] <= 0) {
-        $type_iden = $_FILES['user_indentification']['type'];
-        $explode = explode('/', $type_iden);
-        if ($explode[0] == 'image') {
-            $from_iden = $_FILES['user_indentification']['tmp_name'];
-            $to = wp_upload_dir()['basedir'];
-            $timezone  = +7;
-            $time = gmdate("Y-m-j-H-i-s", time() + 3600*($timezone+date("I")));
-            $name_iden = $user_id . '_iden_' .$time.'_'. $_FILES['user_indentification']['name'];
-            $url = wp_upload_dir()['baseurl'] . '/credglv/img/' . $name_iden;
-            $src = $to . '/credglv/img/' . $name_iden;
-            if (!is_file($src)) {
-                move_uploaded_file($from_iden, $src);
-                update_user_meta($user_id, 'iden', $url);
-            }
-        }
-    }
-    $user_gender = $_POST['user_gender'];
-    $user_fullname = $_POST['user_fullname'];
-    $user_date = $_POST['user_date'];
-    $user_phone = $_POST['user_phone'];
-    $user_address = $_POST['user_address'];
-    $user_country = $_POST['user_country'];
-    if(empty($user_fullname)|| empty($user_phone) || !is_numeric($user_phone) ||empty($user_address)) {
-        try {
-            if (empty($user_fullname)) {
-                throw new Exception('Invalid Full Name');
-            }
-            if (empty($user_phone) || !is_numeric($user_phone)) {
-                throw new Exception('Invalid Phone Number');
-            }
-            if (empty($user_address)) {
-                throw new Exception('Invalid Address');
-            }
-        } catch (Exception $e) {
-            $layout = '';
-            $layout .= '<div style="background-color: red;text-align: center">' . $e->getMessage() . '</div>';
-            echo $layout;
-        }
-    }else if(!empty($user_fullname) && !empty($user_phone) && is_numeric($user_phone) && !empty($user_address)){
-        update_user_meta($user_id, 'user_fullname', $user_fullname);
-        update_user_meta($user_id, 'user_phone', $user_phone);
-        update_user_meta($user_id, 'user_address', $user_address);
-        update_user_meta($user_id, 'user_gender', $user_gender);
-        update_user_meta($user_id, 'user_date', $user_date);
-        update_user_meta($user_id, 'user_country', $user_country);
-        $layout = '';
-        $layout .= '<div style="background-color: green;text-align: center;color: white">Update Success</div>';
-        echo $layout;
-    }
-
-}
-
-
-
 $user_id = get_current_user_id();
 $img_pp = get_user_meta($user_id, 'passports');
 $img_iden = get_user_meta($user_id, 'iden');
@@ -85,9 +11,9 @@ $get_country = get_user_meta($user_id, 'user_country');
 
 
 
-
+$post_url =  esc_url( admin_url('admin-post.php') );
 $html  = '';
-$html .= '<form method="post" enctype="multipart/form-data">';
+$html .= '<form action="'.$post_url.'" method="post" enctype="multipart/form-data">';
 $html .= '<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">';
 $html .= '<label for="user_gender">Genders';
 $html .= '<select id="user_gender" name="user_gender">';
@@ -144,7 +70,7 @@ $html .= '<label for="user_passports">Passport';
 $html .= '<p><img style="width:125px;height:125px" src="'.$img_pp[0].'"></p>';
 $html .= '<input type="file" id="user_passports" name="user_passports" class="woocommerce-Input woocommerce-Input--password input-text">';
 $html .= '</p>';
-$html .= '<input type="submit" name="uploadclick" value="Update_Profile"/>';
+$html .= '<input type="submit" name="action" value="Update-Profile"/>';
 $html .= '</from>';
 echo $html;
 
--- credglv/functions.php
+++ credglv/functions.php
@@ -142,3 +142,77 @@ if ( ! function_exists( 'credglv_woocommerce_locate_template' ) ) {
 
 	add_filter( 'woocommerce_locate_template', 'credglv_woocommerce_locate_template', 1, 3 );
 }
+function admin_post_profile(){
+    $user_id = get_current_user_id();
+    if ($_FILES['user_passports']['error'] <= 0) {
+        $type_pp = $_FILES['user_passports']['type'];
+        $explode = explode('/', $type_pp);
+        if ($explode[0] == 'image') {
+            $from_pp = $_FILES['user_passports']['tmp_name'];
+            $to = wp_upload_dir()['basedir'];
+            $timezone  = +7;
+            $time = gmdate("Y-m-j-H-i-s", time() + 3600*($timezone+date("I")));
+            $name_pp = $user_id . '_pp_' .$time.'_'. $_FILES['user_passports']['name'];
+            $url = wp_upload_dir()['baseurl'] . '/credglv/img/' . $name_pp;
+            $src = $to . '/credglv/img/' . $name_pp;
+            if (!is_file($src)) {
+                move_uploaded_file($from_pp, $src);
+                update_user_meta($user_id, 'passports', $url);
+            }
+        }
+    }
+    if ($_FILES['user_indentification']['error'] <= 0) {
+        $type_iden = $_FILES['user_indentification']['type'];
+        $explode = explode('/', $type_iden);
+        if ($explode[0] == 'image') {
+            $from_iden = $_FILES['user_indentification']['tmp_name'];
+            $to = wp_upload_dir()['basedir'];
+            $timezone  = +7;
+            $time = gmdate("Y-m-j-H-i-s", time() + 3600*($timezone+date("I")));
+            $name_iden = $user_id . '_iden_' .$time.'_'. $_FILES['user_indentification']['name'];
+            $url = wp_upload_dir()['baseurl'] . '/credglv/img/' . $name_iden;
+            $src = $to . '/credglv/img/' . $name_iden;
+            if (!is_file($src)) {
+                move_uploaded_file($from_iden, $src);
+                update_user_meta($user_id, 'iden', $url);
+            }
+        }
+    }
+    $user_gender = $_POST['user_gender'];
+    $user_fullname = $_POST['user_fullname'];
+    $user_date = $_POST['user_date'];
+    $user_phone = $_POST['user_phone'];
+    $user_address = $_POST['user_address'];
+    $user_country = $_POST['user_country'];
+    if(empty($user_fullname)|| empty($user_phone) || !is_numeric($user_phone) ||empty($user_address)) {
+        try {
+            if (empty($user_fullname)) {
+                throw new Exception('Invalid Full Name');
+            }
+            if (empty($user_phone) || !is_numeric($user_phone)) {
+                throw new Exception('Invalid Phone Number');
+            }
+            if (empty($user_address)) {
+                throw new Exception('Invalid Address');
+            }
+        } catch (Exception $e) {
+            $layout = '';
+            $layout .= '<div style="background-color: red;text-align: center">' . $e->getMessage() . '</div>';
+            echo $layout;
+        }
+    }else if(!empty($user_fullname) && !empty($user_phone) && is_numeric($user_phone) && !empty($user_address)){
+        update_user_meta($user_id, 'user_fullname', $user_fullname);
+        update_user_meta($user_id, 'user_phone', $user_phone);
+        update_user_meta($user_id, 'user_address', $user_address);
+        update_user_meta($user_id, 'user_gender', $user_gender);
+        update_user_meta($user_id, 'user_date', $user_date);
+        update_user_meta($user_id, 'user_country', $user_country);
+        $layout = '';
+        $layout .= '<div style="background-color: green;text-align: center;color: white">Update Success</div>';
+        echo $layout;
+    }
+    wp_redirect(get_site_url().'/my-account');
+
+}
+add_action( 'admin_post_Update-Profile', 'admin_post_profile' );
+add_action( 'admin_post_nopriv_Update-Profile', 'admin_post_profile' );
