acc872deeef423909489b73727df9b45a4d931b2
Khoa Vu <nhockunbi131097@gmail.com>
2019-05-07 14:27:13 +0700

    update fix empty get db variable

--- credglv/front/views/user/payment.php
+++ credglv/front/views/user/payment.php
@@ -40,33 +40,80 @@ if(isset($_POST['paymentclick'])){
     }
 }
 
-$get_bankname= get_user_meta($user_id,'bank_name');
-$get_bankbranch = get_user_meta($user_id,'bank_branch');
-$get_bankcountry = get_user_meta($user_id,'bank_country');
-$get_bankowner = get_user_meta($user_id,'bank_owner');
-$get_banknumber = get_user_meta($user_id,'bank_number');
+function get_bankname (){
+    $user_id = get_current_user_id();
+    if (sizeof(get_user_meta($user_id,'bank_name')) == 0) {
+        $get_bankname = [''];
+    } else {
+        $get_bankname = get_user_meta($user_id,'bank_name');
+    }
+    return $get_bankname[0];
+}
+function get_bankbranch (){
+    $user_id = get_current_user_id();
+    if (sizeof(get_user_meta($user_id,'bank_branch')) == 0) {
+        $get_bankbranch = [''];
+    } else {
+        $get_bankbranch = get_user_meta($user_id,'bank_branch');
+    }
+    return $get_bankbranch[0];
+}
+function get_bankcountry (){
+    $user_id = get_current_user_id();
+    if (sizeof(get_user_meta($user_id,'bank_country')) == 0) {
+        $get_bankcountry = [''];
+    } else {
+        $get_bankcountry = get_user_meta($user_id,'bank_country');;
+    }
+    return $get_bankcountry[0];
+}
+function get_bankowner (){
+    $user_id = get_current_user_id();
+    if (sizeof(get_user_meta($user_id,'bank_owner')) == 0) {
+        $get_bankowner = [''];
+    } else {
+        $get_bankowner = get_user_meta($user_id,'bank_owner');;
+    }
+    return $get_bankowner[0];
+}
+function get_banknumber (){
+    $user_id = get_current_user_id();
+    if (sizeof(get_user_meta($user_id,'bank_number')) == 0) {
+        $get_banknumber = [''];
+    } else {
+        $get_banknumber = get_user_meta($user_id,'bank_number');
+    }
+    return $get_banknumber[0];
+}
+
+$get_bankname= get_bankname();
+$get_bankbranch = get_bankbranch();
+$get_bankcountry = get_bankcountry();
+$get_bankowner = get_bankowner();
+$get_banknumber = get_banknumber();
+
 $html = '';
 $html .= '<form method="post">';
 $html .= '<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">';
 $html .= '<label for="user_bankname">Bank Name';
-$html .= '<input type="text" id="user_bankname" name="user_bankname" value="'.$get_bankname[0].'" class="woocommerce-Input woocommerce-Input--password input-text">';
+$html .= '<input type="text" id="user_bankname" name="user_bankname" value="'.$get_bankname.'" class="woocommerce-Input woocommerce-Input--password input-text">';
 $html .= '</p>';
 $html .= '<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">';
 $html .= '<label for="user_bankbranch">Bank Branch';
-$html .= '<input type="text" id="user_bankbranch" name="user_bankbranch" value="'.$get_bankbranch[0].'" class="woocommerce-Input woocommerce-Input--password input-text">';
+$html .= '<input type="text" id="user_bankbranch" name="user_bankbranch" value="'.$get_bankbranch.'" class="woocommerce-Input woocommerce-Input--password input-text">';
 $html .= '</p>';
 $html .= '<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">';
 $html .= '<label for="user_bankcountry">Country of bank';
-$html .= '<input type="text" id="user_bankcountry" name="user_bankcountry" value="'.$get_bankcountry[0].'" class="woocommerce-Input woocommerce-Input--password input-text">';
+$html .= '<input type="text" id="user_bankcountry" name="user_bankcountry" value="'.$get_bankcountry.'" class="woocommerce-Input woocommerce-Input--password input-text">';
 $html .= '</p>';
 $html .= '<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">';
 $html .= '<label for="user_bankowner">Bank Owner';
-$html .= '<input type="text" id="user_bankowner" name="user_bankowner" value="'.$get_bankowner[0].'" class="woocommerce-Input woocommerce-Input--password input-text">';
+$html .= '<input type="text" id="user_bankowner" name="user_bankowner" value="'.$get_bankowner.'" class="woocommerce-Input woocommerce-Input--password input-text">';
 $html .= '</p>';
 $html .= '<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">';
 $html .= '<label for="user_banknumber">Bank Number';
-$html .= '<input type="text" id="user_banknumber" name="user_banknumber" value="'.$get_banknumber[0].'" class="woocommerce-Input woocommerce-Input--password input-text">';
+$html .= '<input type="text" id="user_banknumber" name="user_banknumber" value="'.$get_banknumber.'" class="woocommerce-Input woocommerce-Input--password input-text">';
 $html .= '</p>';
-$html .= '<input type="submit" name="paymentclick" value="Update_Payment"/>';
+$html .= '<input type="submit" name="paymentclick" value="Update"/>';
 $html .= '</from>';
 echo $html;
--- credglv/front/views/user/profile.php
+++ credglv/front/views/user/profile.php
@@ -71,19 +71,89 @@ if (isset($_POST['uploadclick'])) {
 
 }
 
+function get_img_pp(){
+    $user_id = get_current_user_id();
+    if (sizeof(get_user_meta($user_id, 'passports')) == 0) {
+        $get_img_pp = [''];
+    } else {
+        $get_img_pp = get_user_meta($user_id, 'passports');
+    }
+    return $get_img_pp[0];
+}
+function get_img_iden(){
+    $user_id = get_current_user_id();
+    if (sizeof(get_user_meta($user_id, 'iden')) == 0) {
+        $get_img_iden = [''];
+    } else {
+        $get_img_iden = get_user_meta($user_id, 'iden');
+    }
+    return $get_img_iden[0];
+}
+function get_gender()
+{
+    $user_id = get_current_user_id();
+    if (sizeof(get_user_meta($user_id, 'user_gender')) == 0) {
+        $get_gender = [''];
+    } else {
+        $get_gender = get_user_meta($user_id, 'user_gender');
+    }
+    return $get_gender;
+}
+function get_address()
+{
+    $user_id = get_current_user_id();
+    if (sizeof(get_user_meta($user_id, 'user_address')) == 0) {
+        $get_address = [''];
+    } else {
+        $get_address = get_user_meta($user_id, 'user_address');
+    }
+    return $get_address[0];
+}
+function get_fullname(){
+    $user_id = get_current_user_id();
+    if (sizeof(get_user_meta($user_id, 'user_fullname')) == 0) {
+        $get_fullname = [''];
+    } else {
+        $get_fullname = get_user_meta($user_id, 'user_fullname');
+    }
+    return $get_fullname[0];
+}
+function get_date(){
+    $user_id = get_current_user_id();
+    if (sizeof(get_user_meta($user_id, 'user_date')) == 0) {
+        $get_date = [''];
+    } else {
+        $get_date = get_user_meta($user_id, 'user_date');
+    }
+    return $get_date[0];
+}
+function get_phone(){
+    $user_id = get_current_user_id();
+    if (sizeof(get_user_meta($user_id, 'user_phone')) == 0) {
+        $get_phone = [''];
+    } else {
+        $get_phone = get_user_meta($user_id, 'user_phone');
+    }
+    return $get_phone[0];
+}
+function get_country(){
+    $user_id = get_current_user_id();
+    if (sizeof(get_user_meta($user_id, 'user_country')) == 0) {
+        $get_country = [''];
+    } else {
+        $get_country = get_user_meta($user_id, 'user_country');
+    }
+    return $get_country[0];
+}
 
-
-$user_id = get_current_user_id();
-$img_pp = get_user_meta($user_id, 'passports');
-$img_iden = get_user_meta($user_id, 'iden');
-$get_gender = get_user_meta($user_id, 'user_gender');
-$get_fullname = get_user_meta($user_id, 'user_fullname');
-$get_date = get_user_meta($user_id, 'user_date');
-$get_phone = get_user_meta($user_id, 'user_phone');
-$get_address = get_user_meta($user_id, 'user_address');
-$get_country = get_user_meta($user_id, 'user_country');
-
-
+$get_img_pp = get_img_pp();
+$get_img_iden = get_img_iden();
+$get_gender = get_gender();
+$get_address = get_address();
+$get_fullname = get_fullname();
+$get_date = get_date();
+$get_phone = get_phone();
+$get_country = get_country();
 
 
 $html  = '';
@@ -93,7 +163,7 @@ $html .= '<label for="user_gender">Genders';
 $html .= '<select id="user_gender" name="user_gender">';
 $args = ['Mr.','Mrs.','Ms.'];
 for($i=0;$i<count($args);$i++){
-    if($args[$i]==$get_gender[0]){
+    if($args[$i]==$get_gender){
         $html .='<option selected="selected">'.$args[$i].'</option>';
     }
     else{
@@ -105,26 +175,26 @@ $html .= '</select>';
 $html .= '</p>';
 $html .= '<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">';
 $html .= '<label for="user_fullname">Full Name';
-$html .= '<input type="text" id="user_fullnames" name="user_fullname" value="'.$get_fullname[0].'" class="woocommerce-Input woocommerce-Input--password input-text">';
+$html .= '<input type="text" id="user_fullnames" name="user_fullname" value="'.$get_fullname.'" class="woocommerce-Input woocommerce-Input--password input-text">';
 $html .= '</p>';
 $html .= '<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">';
 $html .= '<label for="user_date">Date of Birth';
-$html .= '<input type="date" id="user_date" name="user_date" value="'.$get_date[0].'" class="woocommerce-Input woocommerce-Input--password input-text">';
+$html .= '<input type="date" id="user_date" name="user_date" value="'.$get_date.'" class="woocommerce-Input woocommerce-Input--password input-text">';
 $html .= '</p>';
 $html .= '<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">';
 $html .= '<label for="user_fullname">Phone Number';
-$html .= '<input type="text" id="user_phone" name="user_phone" value="'.$get_phone[0].'" class="woocommerce-Input woocommerce-Input--password input-text">';
+$html .= '<input type="text" id="user_phone" name="user_phone" value="'.$get_phone.'" class="woocommerce-Input woocommerce-Input--password input-text">';
 $html .= '</p>';
 $html .= '<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">';
 $html .= '<label for="user_fullname">Address';
-$html .= '<input type="text" id="user_address" name="user_address" value="'.$get_address[0].'" class="woocommerce-Input woocommerce-Input--password input-text">';
+$html .= '<input type="text" id="user_address" name="user_address" value="'.$get_address.'" class="woocommerce-Input woocommerce-Input--password input-text">';
 $html .= '</p>';
 $html .= '<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">';
 $html .= '<label for="user_country">Country';
 $html .= '<select id="user_country" name="user_country">';
 $arg = ['Vietnam','Cambodia','Indonesia','Indonesia','Myanmar','Philippines','Singapore','Malaysia','Thailand'];
 for($i=0;$i<count($arg);$i++){
-    if($arg[$i]==$get_country[0]){
+    if($arg[$i]==$get_country){
         $html .='<option selected="selected">'.$arg[$i].'</option>';
     }
     else{
@@ -136,15 +206,15 @@ $html .= '</select>';
 $html .= '</p>';
 $html .= '<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">';
 $html .= '<label for="user_indentification">Identification Card';
-$html .= '<p><img style="width:125px;height:125px" src="'.$img_iden[0].'"></p>';
+$html .= '<p><img style="width:125px;height:125px" src="'.$get_img_iden.'"></p>';
 $html .= '<input type="file" id="user_indentification" name="user_indentification" class="woocommerce-Input woocommerce-Input--password input-text">';
 $html .= '</p>';
 $html .= '<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">';
 $html .= '<label for="user_passports">Passport';
-$html .= '<p><img style="width:125px;height:125px" src="'.$img_pp[0].'"></p>';
+$html .= '<p><img style="width:125px;height:125px" src="'.$get_img_pp.'"></p>';
 $html .= '<input type="file" id="user_passports" name="user_passports" class="woocommerce-Input woocommerce-Input--password input-text">';
 $html .= '</p>';
-$html .= '<input type="submit" name="uploadclick" value="Update_Profile"/>';
+$html .= '<input type="submit" name="uploadclick" value="Update Profile"/>';
 $html .= '</from>';
 echo $html;
 
