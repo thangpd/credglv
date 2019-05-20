9ff435c7d9ee65778bdc64bc7cfe1002b89d5e93
thangpd <5cm.1s.takaki@gmail.com>
2019-05-20 15:35:46 +0700

    update html

--- credglv/front/controllers/MycredController.php
+++ credglv/front/controllers/MycredController.php
@@ -148,7 +148,6 @@ class MycredController extends FrontController implements FrontControllerInterfa
 
 				<div class="form-group select-recipient-wrapper">
 				<label>' . __( "Pin", "credglv" ) . '</label>
-				<br>
 				<input type="password" maxlength="4" name="mycred_new_transfer[pin_transfer]" 
 				value="" aria-required="true" class="form-control" >
 				</div>
