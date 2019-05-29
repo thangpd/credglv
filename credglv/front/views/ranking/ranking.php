62d75a7648d6ec742d00fa4bb75a0c384c45058f
thangpd <5cm.1s.takaki@gmail.com>
2019-05-30 06:16:49 +0700

    show logo ranking

--- credglv/front/views/ranking/ranking.php
+++ credglv/front/views/ranking/ranking.php
@@ -1,5 +1,5 @@
 <?php
-echo do_shortcode( '[mycred_my_rank]' );
+echo do_shortcode( '[mycred_my_rank show_logo=1]' );
 //echo do_shortcode( '[mycred_users_of_rank]' );
 //echo do_shortcode( '[mycred_users_of_all_ranks]' );
-echo do_shortcode( '[mycred_list_ranks]' );
+echo do_shortcode( '[mycred_list_ranks show_logo=1]' );
