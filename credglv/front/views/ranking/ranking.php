0706d3014ff9533289606e8daa4dd1a891a10c6b
thangpd <5cm.1s.takaki@gmail.com>
2019-05-30 05:59:57 +0700

    ranking

---
+++ credglv/front/views/ranking/ranking.php
@@ -0,0 +1,5 @@
+<?php
+echo do_shortcode( '[mycred_my_rank]' );
+//echo do_shortcode( '[mycred_users_of_rank]' );
+//echo do_shortcode( '[mycred_users_of_all_ranks]' );
+echo do_shortcode( '[mycred_list_ranks]' );
