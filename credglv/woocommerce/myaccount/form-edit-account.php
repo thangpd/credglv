4e3cea8b426a37df58b701ec2723a6eecd62d133
thangpd <5cm.1s.takaki@gmail.com>
2019-05-31 00:55:50 +0700

    update edit account

--- credglv/woocommerce/myaccount/form-edit-account.php
+++ credglv/woocommerce/myaccount/form-edit-account.php
@@ -51,7 +51,7 @@ do_action( 'woocommerce_before_edit_account_form' ); ?>
     <p class="woocommerce-form-row woocommerce-form-row--last form-row form-row-last">
         <label for="cred_user_pin">
 			<?php _e( 'Pin', 'woocommerce' );
-			echo get_user_meta( $user->data->ID, \credglv\front\controllers\UserController::METAKEY_PIN, true )
+
 			?>
             <span
                     class="required">*</span>
@@ -59,7 +59,7 @@ do_action( 'woocommerce_before_edit_account_form' ); ?>
 
         <input type="password" class="input-text"
                name="<?php echo \credglv\front\controllers\UserController::METAKEY_PIN; ?>" id="cred_user_pin"
-               value=""
+               value="<?php echo get_user_meta( $user->data->ID, \credglv\front\controllers\UserController::METAKEY_PIN, true ) ?>"
                maxlength="4"/>
     </p>
     <div class="clear"></div>
