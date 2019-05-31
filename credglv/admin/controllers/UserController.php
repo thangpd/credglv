718a7539d089c9b0f02867aaa081e19012996540
thangpd <5cm.1s.takaki@gmail.com>
2019-06-01 04:15:30 +0700

    update user edit

--- credglv/admin/controllers/UserController.php
+++ credglv/admin/controllers/UserController.php
@@ -45,74 +45,6 @@ class UserController extends AdminController implements AdminControllerInterface
 		return $val;
 	}
 
-	function save_extra_user_profile_fields( $user_id ) {
-		if ( ! current_user_can( 'edit_user', $user_id ) ) {
-			return false;
-		}
-		if ( isset( $_POST['phone'] ) ) {
-			update_user_meta( $user_id, 'phone', $_POST['phone'] );
-		}
-	}
-
-
-	/**
-	 * @param $redirect_to
-	 * @param $request
-	 * @param $user
-	 *
-	 * @return bool
-	 */
-	public function custom_user_profile_fields( $profileuser ) {
-
-		$user_referrer      = get_the_author_meta( 'referrer', $profileuser->ID );
-		$user_referrer      = get_userdata( $user_referrer );
-		$user_referrer      = ! empty( $user_referrer ) ? $user_referrer->user_nicename : __( 'You\'re the top', 'credglv' );
-		$user_referrer_link = get_user_meta( $profileuser->ID, 'referrer_unikey', true );
-		$user_phone         = get_user_meta( $profileuser->ID, 'phone', true );
-		$user_phone         = ! empty( $user_phone ) ? $user_phone : '';
-		$user_referrer_link = home_url( '?ref=' . $user_referrer_link );
-		?>
-
-        <table class="form-table">
-            <tr>
-                <th>
-                    <label for="phone"><?php esc_html_e( 'Phone', 'credglv' ); ?></label>
-                </th>
-                <td>
-                    <input type="number" name="phone" id="phone"
-                           value="<?php echo esc_attr( $user_phone ); ?>"
-                           class="regular-text"/>
-                    <br><span class="description"><?php esc_html_e( 'Your phone', 'credglv' ); ?></span>
-                </td>
-            </tr>
-            <tr>
-                <th>
-                    <label for="referrer"><?php esc_html_e( 'Your referrer', 'credglv' ); ?></label>
-                </th>
-                <td>
-                    <input type="text" name="referrer" id="referrer" disabled
-                           value="<?php echo esc_attr( $user_referrer ); ?>"
-                           class="regular-text"/>
-                    <br><span class="description"><?php esc_html_e( 'Your referrer', 'credglv' ); ?></span>
-                </td>
-            </tr>
-            <tr>
-                <th>
-                    <label for="user_location"><?php esc_html_e( 'Your invite link', 'credglv' ); ?></label>
-                </th>
-                <td>
-                    <input type="text" name="referrer_link" id="referrer_link"
-                           onClick="this.setSelectionRange(0, this.value.length)"
-                           value="<?php echo esc_attr( $user_referrer_link ); ?>"
-                           class="regular-text"/>
-                    <br><span class="description"><?php esc_html_e( 'Link', 'credglv' ); ?></span>
-                </td>
-            </tr>
-        </table>
-
-		<?php
-	}
-
 
 	public function ajax_active_user() {
 		if ( isset( $_POST['user_id'] ) ) {
@@ -142,11 +74,45 @@ class UserController extends AdminController implements AdminControllerInterface
 		}
 	}
 
+	function extra_user_profile_fields( $user ) { ?>
+        <h3><?php _e( "CREDGLV INFORMATION", "blank" ); ?></h3>
+
+        <table class="form-table">
+            <tr>
+                <th><label for="phone"><?php _e( "PHONE" ); ?></label></th>
+                <td>
+                    <input type="text" name="<?php echo \credglv\front\controllers\UserController::METAKEY_PHONE ?>"
+                           id="<?php echo \credglv\front\controllers\UserController::METAKEY_PHONE ?>"
+                           value="<?php echo esc_attr( get_user_meta( $user->data->ID, \credglv\front\controllers\UserController::METAKEY_PHONE, true ) ); ?>"
+                           class="regular-text"/><br/>
+                    <span class="description"><?php _e( "Please enter your phone here." ); ?></span>
+                </td>
+            </tr>
+        </table>
+	<?php }
+
+	function save_extra_user_profile_fields( $user_id ) {
+		if ( ! current_user_can( 'edit_user', $user_id ) ) {
+			return false;
+		}
+		update_user_meta( $user_id, \credglv\front\controllers\UserController::METAKEY_PHONE, $_POST[ \credglv\front\controllers\UserController::METAKEY_PHONE ] );
+	}
+
 	/**
 	 * Register all actions that controller want to hook
 	 * @return mixed
 	 */
 	public static function registerAction() {
+
+		//show
+		add_action( 'show_user_profile', [ self::getInstance(), 'extra_user_profile_fields' ] );
+		add_action( 'edit_user_profile', [ self::getInstance(), 'extra_user_profile_fields' ] );
+
+
+		//save meta field
+		add_action( 'personal_options_update', [ self::getInstance(), 'save_extra_user_profile_fields' ] );
+		add_action( 'edit_user_profile_update', [ self::getInstance(), 'save_extra_user_profile_fields' ] );
+
 
 		return [
 			'ajax'    => [
