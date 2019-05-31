9ee90cdfbdddd5a834a21885543a3536a98461ff
Thiên Tùng <erik11111@gmail.com>
2019-06-01 00:58:29 +0700

    Revert "update UI redeem"
    This reverts commit 1197d17cf371491affb4222171ce425d6be3aeb6.

--- credglv/front/views/user/redeem_local.php
+++ credglv/front/views/user/redeem_local.php
@@ -3,15 +3,15 @@
 $total_cash  = isset( $data['total_cash']->total ) ? number_format( $data['total_cash']->total, 2, '.', '' ) : 0;
 $total_local = isset( $data['total_local']->total ) ? number_format( $data['total_local']->total, 2, '.', '' ) : 0;
 ?>
-<h3>Cash Balance: <?php
+<h2>Cash Balance: <?php
 	if ( ! empty( $data ) ) {
 		echo $total_cash.' '. __( 'USD', 'credglv' );
-	} ?></h3>
+	} ?></h2>
 <form method="POST" action="" class="form-control form-redeem local-redeem">
 
     <label for="redeem_cash">
 		<?php echo __( 'Local Bank Redeem', 'credglv' ) ?>
-        <input type="text" name="amount" id="amount" style="width: 100%">
+        <input type="text" name="amount" id="amount">
     </label>
     <input type="hidden" name="type" value="local" id="type">
 
