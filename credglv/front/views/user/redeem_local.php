818d2beb901261b5db57874174f04f9095076b04
Thiên Tùng <erik11111@gmail.com>
2019-06-07 14:56:06 +0700

    add text no transaction

--- credglv/front/views/user/redeem_local.php
+++ credglv/front/views/user/redeem_local.php
@@ -31,7 +31,7 @@ if ( ! empty( $data ) ) {
 	?>
 
     <h3>Recent History</h3>
-
+    <?php if($data['html']) { ?>
     <section class="the-css-at-table">
         <header style="display: none;">
             <p class="tr">
@@ -49,7 +49,9 @@ if ( ! empty( $data ) ) {
 
         </div>
     </section>
-	<?php
+	<?php } else {
+        echo '<label>No transaction was made yet</label>';    
+    }
 }
 
 ?>
