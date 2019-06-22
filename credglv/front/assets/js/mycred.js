c690eedf4ee0fbe8ad86abc385b94bb4b67cabad
Jose Cao <thanhcaoxuan1994@gmail.com>
2019-06-22 13:50:31 +0700

    #49 : Fix input value of username on register page

--- credglv/front/assets/js/mycred.js
+++ credglv/front/assets/js/mycred.js
@@ -10,13 +10,7 @@ jQuery(function ($) {
                 if (e.charCode > 47 && e.charCode < 58) {
                     return true;
                 }
-                if (e.charCode > 64 && e.charCode < 91) {
-                    return true;
-                }
                 if (e.charCode > 96 && e.charCode < 123) {
-                    return true;
-                }
-                if (e.charCode === 8){
                     return true;
                 }
                 return false;
