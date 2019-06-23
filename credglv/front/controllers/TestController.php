bb46e808ab4d0adc53d1221fb252ad38b2a2dd79
Thiên Tùng <erik11111@gmail.com>
2019-06-23 10:57:24 +0700

    test

---
+++ credglv/front/controllers/TestController.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * @copyright © 2019 by GLV
+ * @project Cred GLV Plugin
+ *
+ * @since 1.0
+ *
+ */
+
+
+namespace credglv\front\controllers;
+
+use credglv\core\components\RoleManager;
+use credglv\models\UserModel;
+use credglv\core\interfaces\FrontControllerInterface;
+use http\Client\Curl\User;
+use PHPUnit\Runner\Exception;
+
+
+class TestController extends FrontController implements FrontControllerInterface {
+
+
+	function credglv_assets_enqueue() {
+		global $post, $wp_query;
+
+
+	}
+
+	public function registerPage() {
+		$data = [];
+
+		return $this->render( 'ranking', [ 'data' => $data ] );
+	}
+
+	/**
+	 * Register all actions that controller want to hook
+	 * @return mixed
+	 */
+	public static function registerAction() {
+
+
+		return [
+			'actions' => [
+
+				'wp_enqueue_scripts'              => [ self::getInstance(), 'credglv_assets_enqueue' ],
+			],
+			'ajax'    => [
+
+			],
+			'pages'   => [
+				'front' => [
+					'test_controller' =>
+						[
+							'registerPage',
+							[
+								'title' => __( 'test controller', 'credglv' ),
+//                                'single' => true
+							]
+						],
+
+				]
+			],
+			'assets'  => [
+				'js'  => [
+					/*[
+						'id'       => 'credglv-register-page-js',
+						'isInline' => false,
+						'url'      => '/front/assets/js/register.js',
+					],*/
+					[
+						'id'       => 'credglv-main-js',
+						'isInline' => false,
+						'url'      => '/front/assets/js/main.js',
+					]
+				]
+			]
+		];
+	}
+
+}
