743a2ea039811ff241ec426a433f7746370209d2
no <leno2796@gmail.com>
2019-06-24 23:01:42 +0700

    update placeholder form input

--- credglv/woocommerce/myaccount/form-login.php
+++ credglv/woocommerce/myaccount/form-login.php
@@ -36,7 +36,7 @@ do_action( 'woocommerce_before_customer_login_form' ); ?>
 			<?php do_action( 'woocommerce_login_form_start' ); ?>
             <div class="myaccount-login-page hide">
                 <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
-                    <input type="text" class="woocommerce-Input woocommerce-Input--text input-text" placeholder="Username or email address" name="username" autocapitalize="none" pattern="[a-z]*"
+                    <input type="text" class="woocommerce-Input woocommerce-Input--text input-text us" placeholder="Username or email address" name="username" autocapitalize="none" pattern="[a-z]*"
                            id="username" autocomplete="username"
                            value="<?php echo ( ! empty( $_POST['username'] ) ) ? esc_attr( wp_unslash( $_POST['username'] ) ) : ''; ?>"/><?php // @codingStandardsIgnoreLine ?>
                 </p>
