69ea99f721002bfe5ea85b1e39b199f15818aa24
Thiên Tùng <erik11111@gmail.com>
2019-06-24 22:56:47 +0700

    update server key

--- credglv/front/controllers/PushNotifyController.php
+++ credglv/front/controllers/PushNotifyController.php
@@ -63,7 +63,7 @@ class PushNotifyController extends FrontController implements FrontControllerInt
 		);
 
 		$headers = array(
-			'Authorization: key=AAAAkjudY3w:APA91bG4qRIbhZ-8nfi3Lrt2juVaFC8GQo_CNoPXMcKPDM_Tm5tNetB_IQIm890cS7McgqHbRlIZ2XjD2pR1o7IevdJFiiN825XDl5nAgT8OXk-i2yPNPfme2nirI0Xe2yZ0Liw25dGN',
+			'Authorization: key=AAAAezkvKa4:APA91bGOxymJnloNCZRFaVOB5X2_OREKfmFgczOZ_Ds2dYvOixbV8zZ7EHRlLGof9zMzO4I9ggrzsWYH1H1tptICZXD0yT7WZFgzj578-fqvbFyOu9IsrfWemNZTBMYJg3tjGlteN8_N',
 			'Content-Type: application/json'
 		);
 		 
