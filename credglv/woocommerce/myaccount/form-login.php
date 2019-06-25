dd8ae5d1bbc3ea64c0e79abc25f445dd861e7831
no <leno2796@gmail.com>
2019-06-26 02:13:32 +0700

    update

--- credglv/woocommerce/myaccount/form-login.php
+++ credglv/woocommerce/myaccount/form-login.php
@@ -32,7 +32,12 @@ do_action( 'woocommerce_before_customer_login_form' ); ?>
         <!-- <div align="center"><h2><?php //esc_html_e( 'Login', 'woocommerce' ); ?></h2></div> -->
 
         <form class="woocommerce-form woocommerce-form-login login" method="post">
-
+            <p class="form-row form-row-wide f-login-title">
+                <label for="login-with-phone" id="label-login-with-phone" style="display: none"> <input type="radio" id="login-with-phone" name="selector" checked>
+                    <span><?php echo __( 'With phone number', 'credglv' ); ?></span></label>
+                <label for="login-with-user" id="label-login-with-user"> <input type="radio" id="login-with-user" name="selector">
+                    <span><?php echo __( 'With username/email', 'credglv' ); ?></span></label>
+            </p>
 			<?php do_action( 'woocommerce_login_form_start' ); ?>
             <div class="myaccount-login-page hide">
                 <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide f-p-focus mt-10 f-bd">
