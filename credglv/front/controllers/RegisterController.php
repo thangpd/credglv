3d44026dd7e930cb54cc4e2dba714da7c93ada6b
no <leno2796@gmail.com>
2019-06-26 01:22:54 +0700

    update

--- credglv/front/controllers/RegisterController.php
+++ credglv/front/controllers/RegisterController.php
@@ -136,7 +136,7 @@ class RegisterController extends FrontController implements FrontControllerInter
 			$option = '<option value="' . $user->data->ID . '">' . $user->data->user_login . '</option>';
 		}
 		?>
-        <p class="form-row form-row-wide mt-20">
+        <p class="form-row form-row-wide mt-40">
             <label for="reg_referral">
 				<?php _e( 'Introducer', 'credglv' ); ?>
             </label>
